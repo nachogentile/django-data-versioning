265487214ad679a2b3f2479a8aa1ad526781cdc3
Nacho Gentile <nacho.gentile@gameloft.com>
2016-03-14 13:04:25 +0100

    * Changing migrator to work with the generation script

--- setup_db.py
+++ setup_db.py
@@ -3,10 +3,17 @@ from django.conf import settings
 from django.core.management.commands import migrate
 
 
-def create_database(db_name):
+def migrate_db(db_name):
     """
-    Creates and populates a new database based on the name provided
+    Launch migration command for the provided database name.
     """
-    subprocess.call(["createdb", "-U", "postgres", db_name, '-h', settings.PGSQL_SERVER_ADDRESS])
     migrate_cmd = migrate.Command()
     migrate_cmd.execute(database=db_name, app_label=None, interactive=False, verbosity=1)
+
+
+def create_database(db_name):
+    """
+    Creates and populates a new database based on the name provided.
+    """
+    subprocess.call(["createdb", "-U", "postgres", db_name, '-h', settings.PGSQL_SERVER_ADDRESS])
+    migrate_db(db_name)
